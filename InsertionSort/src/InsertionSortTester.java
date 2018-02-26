3b0c3304857d89620fefeef4d1a6ea6937acdc4b
Tai <taidao90@gmail.com>
2018-02-25 19:38:26 -0800

    InsertionSort Finished

---
+++ InsertionSort/src/InsertionSortTester.java
@@ -0,0 +1,30 @@
+/**
+ * "Introduction to Algorithms - Thomas H. Cormen". 3rd edition. (18).
+ *    Pseudocode Translated By: Tai Dao
+ *    Date Written: 02/23/18
+ */
+
+import java.util.Arrays;
+public class InsertionSortTester {
+	public static void main(String[] args) {
+		int[] arrayToSort = {18, 25, 6, 9, 15, 12, 5, 20, 11, 30};
+		int key;
+		int i;
+		
+		for (int j = 1; j < arrayToSort.length ; j++) {
+			key = arrayToSort[j];
+			// Inserting key into sorted Array
+			i = j - 1;
+			while (i >= 0 && arrayToSort[i] > key){
+				arrayToSort[i+1] = arrayToSort[i];
+				i = i - 1;
+			}
+			arrayToSort[i+1] = key;
+			printArray(arrayToSort);
+		}
+	}
+	
+   private static void printArray(int[] array) {
+	   System.out.println(Arrays.toString(array));
+   }
+}
