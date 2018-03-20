3b0a617d43a76eff0b7f60e4838e8d99ac7fc0f0
Tai <taidao90@gmail.com>
2018-03-20 12:56:02 -0700

    Initial Commit

---
+++ PriorityQueue/src/MultiTaskingSimulator.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 
+ * @author Tai Dao
+ *
+ */
+public class MultiTaskingSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+	}
+
+}
