a086ed86ec41ba3a46391ed1af2727b498a880aa
Tai <taidao90@gmail.com>
2018-04-29 02:38:42 -0700

    Do I need to write a linked list from scratch?

--- HashTable/src/ChainedHashTable.java
+++ HashTable/src/ChainedHashTable.java
@@ -8,13 +8,20 @@ public class ChainedHashTable {
 	ChainedHashTable(){
 		hashTable = new LinkedList[11];
 		for (int i = 0; i < 11; i ++) {
-			hashTable[i] = new LinkedList<Process> ();
+			hashTable[i] = new LinkedList<Process> (); // each element of the hash table is initialized to an empty linked list
 		}
 	}
 	
 	public void hashInsert(ChainedHashTable this, Process p) {
-		int hashResult = p.getPriority() % 11;
-		hashTable[hashResult].push(p);
+		int hashResult = p.getPriority() % 11; // this is the hash function
+		hashTable[hashResult].add(p); //A Process is pushed into the LinkedList
+	}
+	
+	public void printHashTable() {
+		for (int i = 0; i < 11; i ++) {
+			hashTable[i].getFirst();
+			
+		}
 	}
 	
 }
