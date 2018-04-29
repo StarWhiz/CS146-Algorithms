fcbbf161c9ecd008de65b6cf31094d86e5138383
Tai <taidao90@gmail.com>
2018-04-29 01:35:06 -0700

    Removed Requirements from Comments

--- BST/src/BstTester.java
+++ BST/src/BstTester.java
@@ -5,16 +5,6 @@ import java.util.Scanner;
  * 
  * @author Tai Dao
  * Date: 04/27/2018
- *
-Requirements
-a) Build up a Process BST. (MUST follow BST properties specified in textbook
-and ppt slides. Your own tree structure will not be accepted.)
-
-b) Insert a process to the BST based on its priority code.
-
-c) Delete a process from the BST.
-
-d) Make a sorted list of processes according to the priority codes from the BST.
 */
 public class BstTester {
 	static Tree t = new Tree();
