cdede72bd01177eee566be9eb7edd7d3ae73d330
Tai <taidao90@gmail.com>
2018-04-29 01:34:26 -0700

    HashTable Initial Commit

---
+++ HashTable/src/Process.java
@@ -0,0 +1,66 @@
+public class Process implements Comparable<Process> {
+	private int priority;
+	private String processID;
+	
+	/** This is a constructor for creating unnamed lowest priority Process
+	 */
+	Process() {
+		priority = 0;
+		processID = null;
+	}
+	
+	
+	/**
+	 * This constructor initializes the priority of the Process when it is created
+	 * with a random number from 1 to 9999.
+	 */
+	Process(int currentPID) {
+		//priority = currentPID +1000; // FOR DEBUGGING
+		priority = (int) (Math.random()*9999 + 1);
+		processID = "P" + currentPID;
+	}
+	
+	
+	/**
+	 * This function returns the priority of the Process
+	 * @return int priority
+	 */
+	public int getPriority (){
+		return priority;
+	}
+	
+	
+	/**
+	 * This function returns the PID of the Process
+	 * @return int priority
+	 */
+	public String getPID (){
+		return processID;
+	}
+	
+	
+	/**
+	 * This function takes an int and sets priority to it
+	 * @param p
+	 */
+	public void setPriority(int p) {
+		priority = p;
+	}
+	
+	/**
+	 * This function takes another Process and compares it's priority with the current process.
+	 * @param Process
+	 */
+	@Override
+	public int compareTo(Process other) {
+		if (priority < other.getPriority()) {
+			return -1;
+		}
+		else if (priority == other.getPriority()) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+}
