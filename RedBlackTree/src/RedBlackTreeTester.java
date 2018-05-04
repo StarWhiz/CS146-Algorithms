c35cc24847513e71fbd2c71085ba063f80cc90a7
Tai <taidao90@gmail.com>
2018-05-04 13:45:20 -0700

    tester loaded with 20 random processes

--- RedBlackTree/src/RedBlackTreeTester.java
+++ RedBlackTree/src/RedBlackTreeTester.java
@@ -1,9 +1,18 @@
 
 public class RedBlackTreeTester {
-
+	static RBTree t = new RBTree();
+	static int currentPID = 0;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Hello World");
+		
+		// creates 20 Processes with random priorities into the Tree 
+		for (int i = 0; i < 20 ; i++) {
+			Process newProcess = new Process(currentPID); 
+			t.processInsert(newProcess);
+			currentPID++;
+		}
 
 	}
 
