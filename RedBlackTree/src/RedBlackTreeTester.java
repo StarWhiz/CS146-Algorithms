410d95b05aff3e5b930570c27e7581c9f788ae72
Tai <taidao90@gmail.com>
2018-05-14 22:34:03 -0700

    added report, and runnable jar

--- RedBlackTree/src/RedBlackTreeTester.java
+++ RedBlackTree/src/RedBlackTreeTester.java
@@ -5,11 +5,7 @@ public class RedBlackTreeTester {
 	static int currentPID = 0;
 	
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
-		
 		// creates 20 Processes with random priorities into the Tree 
-		
 		for (int i = 0; i < 3 ; i++) {
 			Process newProcess = new Process(currentPID); 
 			t.processInsert(newProcess);
